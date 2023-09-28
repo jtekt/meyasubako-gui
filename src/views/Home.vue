0cc0cb65c06e91de8665f4bd55c688c42d8d1f62
Maxime Moreillon <m.moreillon@gmail.com>
2023-09-29 08:29:41 +0900

    basics working

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,42 +1,56 @@
 <template>
   <div class="home">
     <h1>無駄削減</h1>
-    <p>業務効率改善のために、日常業務にある改善できそうなことをここにリストアップしましょう！</p>
-
-    <h2>アイテム追加</h2>
-    <form class="" @submit.prevent="submit()">
-      <input type="text" v-model="monku_content" placeholder="文句内容">
-      <input type="submit">
-    </form>
-
-    <h2>あるアイテム</h2>
-    <p>アイテムをクリックすると対策の提案書けるようになります</p>
-
-    <p class="toolbar">
-      <span v-if="complaints.length > 0">{{complaints.length}} Items</span>
-
-      <span class="spacer"/>
-
-      <span>並べ替え / Sorting: </span>
-
-
-      <button
-        type="button"
-        :class="{active: sorting ==='timestamp' }"
-        @click="sorting='timestamp'">
-        <CalendarIcon />
-      </button>
-
-      <button
-        type="button"
-        :class="{active: sorting ==='likes' }"
-        @click="sorting='likes'">
-        <ThumbUpIcon />
-      </button>
+    <p>
+      業務効率改善のために、日常業務にある改善できそうなことをここにリストアップしましょう！
     </p>
 
+    <h2></h2>
+    <v-form class="" @submit.prevent="submit()">
+      <v-row align="center">
+        <v-col>
+          <v-text-field
+            type="text"
+            v-model="monku_content"
+            label="アイテム追加"
+          />
+        </v-col>
+        <v-col cols="auto">
+          <v-btn type="submit" icon>
+            <v-icon>mdi-send</v-icon>
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-form>
+
+    <v-toolbar flat>
+      <span v-if="complaints.length > 0">
+        <v-icon>mdi-comment</v-icon> x {{ complaints.length }}
+      </span>
+      <v-spacer></v-spacer>
+      <!-- TODO: button group with active -->
+      <v-btn
+        type="button"
+        icon
+        :class="{ active: sorting === 'timestamp' }"
+        @click="sorting = 'timestamp'"
+      >
+        <v-icon>mdi-calendar</v-icon>
+      </v-btn>
+
+      <v-btn
+        type="button"
+        icon
+        :class="{ active: sorting === 'likes' }"
+        @click="sorting = 'likes'"
+      >
+        <v-icon>mdi-thumb-up</v-icon>
+      </v-btn>
+    </v-toolbar>
+    <v-divider />
+
     <div class="loader_container" v-if="loading">
-      <Loader>Loading items</Loader>
+      <v-progress-circular indeterminate />
     </div>
 
     <div class="monku_wrapper" v-else>
@@ -44,104 +58,103 @@
         <Item
           link
           v-for="complaint in sorted_complaints"
-          v-bind:key="complaint._id"
-          v-bind:item="complaint"
+          :key="complaint._id"
+          :item="complaint"
           @vote="vote($event)"
-          @deleteItem="delete_monku($event)"/>
+          @deleteItem="delete_monku($event)"
+        />
       </transition-group>
     </div>
-
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Item from '@/components/Item.vue'
-import Loader from '@moreillon/vue_loader'
-
-import ThumbUpIcon from 'vue-material-design-icons/ThumbUp.vue'
-import CalendarIcon from 'vue-material-design-icons/Calendar.vue'
-
+import Item from "@/components/Item.vue"
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
     Item,
-    Loader,
-    ThumbUpIcon,
-    CalendarIcon,
   },
-  data(){
+  data() {
     return {
-      monku_content: '',
+      monku_content: "",
       complaints: [],
-      sorting: 'timestamp',
+      sorting: "timestamp",
       ordering: 1,
       loading: false,
     }
   },
-  mounted(){
+  mounted() {
     this.get_monku()
   },
   methods: {
-    submit(){
-      if(!confirm('文句を登録しますか？')) return
-      this.axios.post(`${process.env.VUE_APP_MENDOKUSAI_API_URL}/monku`, {
-        content: this.monku_content
-      })
-      .then(() => {
-        this.monku_content = ''
-        this.get_monku()
-      })
-      .catch(error => console.log(error))
+    submit() {
+      if (!confirm("文句を登録しますか？")) return
+      this.axios
+        .post(`${process.env.VUE_APP_MENDOKUSAI_API_URL}/monku`, {
+          content: this.monku_content,
+        })
+        .then(() => {
+          this.monku_content = ""
+          this.get_monku()
+        })
+        .catch((error) => console.log(error))
     },
-    get_monku(){
+    get_monku() {
       this.loading = true
-      this.axios.get(`${process.env.VUE_APP_MENDOKUSAI_API_URL}/monku`)
-      .then(response => {
-        this.complaints.splice(0,this.complaints.length)
-        response.data.forEach((complaint) => {
-          this.complaints.push(complaint)
+      this.axios
+        .get(`${process.env.VUE_APP_MENDOKUSAI_API_URL}/monku`)
+        .then((response) => {
+          this.complaints.splice(0, this.complaints.length)
+          response.data.forEach((complaint) => {
+            this.complaints.push(complaint)
+          })
         })
-      })
-      .catch(error => console.log(error))
-      .finally(() => this.loading = false)
+        .catch((error) => console.log(error))
+        .finally(() => (this.loading = false))
     },
-    vote(data){
+    vote(data) {
       let url = `${process.env.VUE_APP_MENDOKUSAI_API_URL}/monku/${data.id}/vote`
-      this.axios.post(url, {
-        vote: data.vote,
-      })
-      .then((response) => {
-        let found_complaint = this.complaints.find(complaint => { return complaint._id === response.data.value._id
+      this.axios
+        .post(url, {
+          vote: data.vote,
         })
+        .then((response) => {
+          let found_complaint = this.complaints.find((complaint) => {
+            return complaint._id === response.data.value._id
+          })
 
-        if(found_complaint) found_complaint.likes = response.data.value.likes
-      })
-      .catch(error => console.log(error))
+          if (found_complaint) found_complaint.likes = response.data.value.likes
+        })
+        .catch((error) => console.log(error))
     },
-    delete_monku(id){
-      if(confirm(`ホンマに？`)){
+    delete_monku(id) {
+      if (confirm(`ホンマに？`)) {
         let url = `${process.env.VUE_APP_MENDOKUSAI_API_URL}/monku/${id}`
-        this.axios.delete(url)
-        .then(() => {this.get_monku()})
-        .catch(error => console.log(error))
+        this.axios
+          .delete(url)
+          .then(() => {
+            this.get_monku()
+          })
+          .catch((error) => console.log(error))
       }
     },
   },
   computed: {
-    sorted_complaints(){
-
-      if(this.sorting === 'timestamp'){
-        return this.complaints.slice().sort((a, b) => {return new Date (b.timestamp) - new Date (a.timestamp)})
+    sorted_complaints() {
+      if (this.sorting === "timestamp") {
+        return this.complaints.slice().sort((a, b) => {
+          return new Date(b.timestamp) - new Date(a.timestamp)
+        })
+      } else {
+        return this.complaints.slice().sort((a, b) => {
+          return b.likes - a.likes
+        })
       }
-      else {
-        return this.complaints.slice().sort((a, b) => {return b.likes - a.likes})
-      }
-
-    }
-  }
-
+    },
+  },
 }
 </script>
 
@@ -182,5 +195,4 @@ button.active {
 .spacer {
   flex-grow: 1;
 }
-
 </style>
--- src/views/Monku.vue
+++ src/views/Monku.vue
@@ -2,24 +2,23 @@
   <div class="home">
     <h1>アイテム</h1>
 
-
     <div class="loader_container" v-if="loading">
-      <Loader>Loading</Loader>
+      <v-progress-circular indeterminate />
     </div>
 
     <template v-if="monku">
-
       <Item
         v-bind:item="monku"
         @vote="vote_monku($event)"
-        @deleteItem="delete_monku($event)"/>
+        @deleteItem="delete_monku($event)"
+      />
 
       <h2>コメント</h2>
 
       <h3>コメント追加</h3>
       <form class="" @submit.prevent="submit_proposal()">
-        <input type="text" v-model="proposal_content" placeholder="提案内容">
-        <input type="submit" >
+        <input type="text" v-model="proposal_content" placeholder="提案内容" />
+        <input type="submit" />
       </form>
 
       <h3>今までのコメント</h3>
@@ -29,139 +28,138 @@
           v-bind:key="proposal._id"
           v-bind:item="proposal"
           @vote="vote_proposal($event)"
-          @deleteItem="delete_proposal($event)"/>
+          @deleteItem="delete_proposal($event)"
+        />
       </transition-group>
-
     </template>
-
-
-
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 
-import Item from '@/components/Item.vue'
-import Loader from '@moreillon/vue_loader'
+import Item from "@/components/Item.vue"
 
 export default {
-  name: 'Monku',
+  name: "Monku",
   components: {
     Item,
-    Loader,
-
   },
-  data(){
+  data() {
     return {
       monku: null,
-      proposal_content: '',
+      proposal_content: "",
       loading: false,
     }
   },
-  mounted(){
+  mounted() {
     this.get_monku()
     //this.get_proposals()
   },
   methods: {
-    submit_proposal(){
-      if(!confirm('提案を登録しますか？')) return
+    submit_proposal() {
+      if (!confirm("提案を登録しますか？")) return
 
       let url = `${process.env.VUE_APP_MENDOKUSAI_API_URL}/monku/${this.monku._id}/proposals`
-      this.axios.post(url, {
-        content: this.proposal_content,
-      })
-      .then(() => {
-        // Clear input
-        this.proposal_content = ''
-
-        // Reload data
-        this.get_monku()
-      })
-      .catch(error => console.log(error))
-    },
-    get_monku(){
-      this.loading = true
-
-      let url = `${process.env.VUE_APP_MENDOKUSAI_API_URL}/monku/${this.$route.params.monku_id}`
-      this.axios.get(url)
-      .then(response => {
-        this.monku = response.data[0]
-      })
-      .catch(error => console.log(error))
-      .finally(() => this.loading = false)
-
-    },
-    get_proposals(){
-      let url = `${process.env.VUE_APP_MENDOKUSAI_API_URL}/monku/${this.$route.params.monku_id}/proposals`
-
-      this.axios.get(url)
-      .then(response => {
-        console.log(response.data)
-
-      })
-      .catch(error => console.log(error))
-    },
-    vote_monku(data){
-      let url = `${process.env.VUE_APP_MENDOKUSAI_API_URL}/monku/${this.$route.params.monku_id}/vote`
-
-      this.axios.post(url, {
-        vote: data.vote,
-      })
-      .then((response) => {
-        this.monku.likes = response.data.value.likes
-      })
-      .catch(error => console.log(error))
-    },
-    vote_proposal(data){
-      const url = `${process.env.VUE_APP_MENDOKUSAI_API_URL}/proposals/${data.id}/vote`
-
-      this.axios.post(url, {
-        vote: data.vote,
-      })
-      .then((response) => {
-        const found_proposal = this.monku.proposals.find(proposal => proposal._id === response.data.value._id)
-
-        if(found_proposal) found_proposal.likes = response.data.value.likes
-      })
-      .catch(error => console.log(error))
-    },
-    delete_monku(id){
-      if(confirm(`ホンマに？`)){
-        let url = `${process.env.VUE_APP_MENDOKUSAI_API_URL}/monku/${id}`
-        this.axios.delete(url)
-        .then(() => {
-          this.$router.push('/')
+      this.axios
+        .post(url, {
+          content: this.proposal_content,
         })
-        .catch(error => console.log(error))
-      }
-    },
-    delete_proposal(id){
-      if(confirm(`ホンマに？`)){
-        let url = `${process.env.VUE_APP_MENDOKUSAI_API_URL}/proposals/${id}`
-        this.axios.delete(url)
         .then(() => {
+          // Clear input
+          this.proposal_content = ""
+
           // Reload data
           this.get_monku()
         })
-        .catch(error => console.log(error))
+        .catch((error) => console.log(error))
+    },
+    get_monku() {
+      this.loading = true
+
+      let url = `${process.env.VUE_APP_MENDOKUSAI_API_URL}/monku/${this.$route.params.monku_id}`
+      this.axios
+        .get(url)
+        .then((response) => {
+          this.monku = response.data[0]
+        })
+        .catch((error) => console.log(error))
+        .finally(() => (this.loading = false))
+    },
+    get_proposals() {
+      let url = `${process.env.VUE_APP_MENDOKUSAI_API_URL}/monku/${this.$route.params.monku_id}/proposals`
+
+      this.axios
+        .get(url)
+        .then((response) => {
+          console.log(response.data)
+        })
+        .catch((error) => console.log(error))
+    },
+    vote_monku(data) {
+      let url = `${process.env.VUE_APP_MENDOKUSAI_API_URL}/monku/${this.$route.params.monku_id}/vote`
+
+      this.axios
+        .post(url, {
+          vote: data.vote,
+        })
+        .then((response) => {
+          this.monku.likes = response.data.value.likes
+        })
+        .catch((error) => console.log(error))
+    },
+    vote_proposal(data) {
+      const url = `${process.env.VUE_APP_MENDOKUSAI_API_URL}/proposals/${data.id}/vote`
+
+      this.axios
+        .post(url, {
+          vote: data.vote,
+        })
+        .then((response) => {
+          const found_proposal = this.monku.proposals.find(
+            (proposal) => proposal._id === response.data.value._id
+          )
+
+          if (found_proposal) found_proposal.likes = response.data.value.likes
+        })
+        .catch((error) => console.log(error))
+    },
+    delete_monku(id) {
+      if (confirm(`ホンマに？`)) {
+        let url = `${process.env.VUE_APP_MENDOKUSAI_API_URL}/monku/${id}`
+        this.axios
+          .delete(url)
+          .then(() => {
+            this.$router.push("/")
+          })
+          .catch((error) => console.log(error))
       }
     },
-
-
+    delete_proposal(id) {
+      if (confirm(`ホンマに？`)) {
+        let url = `${process.env.VUE_APP_MENDOKUSAI_API_URL}/proposals/${id}`
+        this.axios
+          .delete(url)
+          .then(() => {
+            // Reload data
+            this.get_monku()
+          })
+          .catch((error) => console.log(error))
+      }
+    },
   },
   computed: {
-    sorted_proposals(){
-      return this.monku.proposals.slice().sort((a, b) => {return b.likes - a.likes});
+    sorted_proposals() {
+      return this.monku.proposals.slice().sort((a, b) => {
+        return b.likes - a.likes
+      })
     },
-    user_is_admin(){
-      if(this.$store.state.user){
+    user_is_admin() {
+      if (this.$store.state.user) {
         return !!this.$store.state.user.properties.isAdmin
-      }
-      else return false
-    }
-  }
-
+      } else return false
+    },
+  },
 }
 </script>
 
@@ -169,7 +167,7 @@ export default {
 .monku_wrapper {
   margin-top: 1em;
 }
-.monku{
+.monku {
   display: flex;
   align-items: center;
   padding: 0.5em;
