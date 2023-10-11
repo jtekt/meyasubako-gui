8bc9f64c2f051a0f3d692f228b49464bff6817d2
Maxime Moreillon <m.moreillon@gmail.com>
2023-10-12 08:48:33 +0900

    substitute env

--- substituteEnv.sh
+++ substituteEnv.sh
@@ -4,11 +4,11 @@ ROOT_DIR=/app
 
 # Replace env vars in files served by NGINX
 echo "Replacing environment variables"
-for file in $ROOT_DIR/assets/*.js* $ROOT_DIR/index.html $ROOT_DIR/precache-manifest*.js;
+for file in $ROOT_DIR/assets/*.js* $ROOT_DIR/index.html;
 do
   echo "Processing $file ...";
 
-  sed -i 's|VITE_MEYASUBAKO_API_URL_PLACEHOLDER|'${VITE_MEYASUBAKO_API_URL}'|g' $file
+  sed -i 's|VITE_MEYASUBAKO_API_URL_PLACEHOLDER|'${VITE_MEYASUBAKO_API}'|g' $file
 
 
 
